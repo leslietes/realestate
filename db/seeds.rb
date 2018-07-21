485196f62f06a831a93ab3b4dfa7333021c21245
Leslie T <leslie_tanengsy@yahoo.com>
2018-07-21 23:42:23 +0800

    Edit seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Article.create(title: 'Manhattan Real Estate Feels Summer Slump',
+Location.create!([{area: 'I.T. Park'}, { area: 'Cebu Business Park'}, {area: 'Downtown'}, {area: 'Capitol'}, {area: 'Mango Avenue'}, {area: 'Talamban'}, {area: 'Lahug'}, {area: 'Mabolo'}, {area: 'North Reclamation Area'}, {area: 'SRP'}, {area: 'Gorordo'} ])
+
+Article.create!(title: 'Manhattan Real Estate Feels Summer Slump',
   permalink: 'manhattan-real-estate',
   teaser: 'Manhattan’s real estate market is feeling the lazy days of summer as buyers and sellers head out of town for vacation, leading transactions to fall in the city, according to the weekly Olshan Report on Monday.',
   body: 'Manhattan’s real estate market is feeling the lazy days of summer as buyers and sellers head out of town for vacation, leading transactions to fall in the city, according to the weekly Olshan Report on Monday.',
   published_date: 'July 16, 2018',
   published_at: 'www.mansionglobal.com')
 
-Setting.create(agency_name: "Cebu Condo",
+Setting.create!(agency_name: "Cebu Condo",
   agent_name: 'Mickey Mouse',
   production_email: 'inquiries@cebucondo.ph',
   sales_email: 'sales@cebucondo.ph',
@@ -26,19 +28,23 @@ Setting.create(agency_name: "Cebu Condo",
   about_us: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu rutrum mi, et tempor lorem. Mauris sed efficitur nisi, sit amet vulputate enim. Morbi vel velit id risus accumsan tempus a commodo tortor. Mauris a placerat felis. Aliquam et turpis tincidunt, auctor dolor ut, fringilla erat. Morbi hendrerit mauris urna, et luctus est lacinia et. In hac habitasse platea dictumst. Suspendisse magna turpis, luctus ultricies auctor id, commodo a nunc. Donec augue dolor, posuere sed vestibulum ut, venenatis eget dolor. Praesent vehicula mauris eros, at dictum felis vehicula eu. Sed velit mi, lobortis quis vehicula placerat, cursus pellentesque ligula. Suspendisse ut odio odio. Aliquam in erat non nibh convallis euismod. Duis convallis, dui in imperdiet viverra, nunc tortor iaculis ante, id accumsan mauris nibh ut nisl. Aenean elit ligula, sollicitudin id est eu, lacinia convallis mauris.'
 )
 
-Agent.create(agent_name: 'Mickey Mouse',
+Agent.create!(agent_name: 'Mickey Mouse',
   mobile_no: '0917-123-4567',
   email_address: 'mickeymouse@cebucondo.ph',
   position: 'Manager'
 )
 
-Developer.create(developer: 'Ayala Land', permalink: 'ayala-land')
-Developer.create(developer: 'Cebu Landmasters', permalink: 'cebu-landmasters')
-Developer.create(developer: 'Primary Properties', permalink: 'primary-properties')
+Developer.create!(developer: 'Ayala Land', permalink: 'ayala-land')
+Developer.create!(developer: 'Cebu Landmasters', permalink: 'cebu-landmasters')
+Developer.create!(developer: 'Primary Properties', permalink: 'primary-properties')
 
-Property.create(developer_id: 1, name: 'Avida Land', permalink: 'avida-land', address: 'I.T. Park, Lahug, Cebu City',
-  location: 'Lahug', completed: true, target_completion_date: 'December 2016',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu rutrum mi, et tempor lorem. Mauris sed efficitur nisi, sit amet vulputate enim. Morbi vel velit id risus accumsan tempus a commodo tortor. Mauris a placerat felis. Aliquam et turpis tincidunt, auctor dolor ut, fringilla erat. Morbi hendrerit mauris urna, et luctus est lacinia et. In hac habitasse platea dictumst. Suspendisse magna turpis, luctus ultricies auctor id, commodo a nunc. Donec augue dolor, posuere sed vestibulum ut, venenatis eget dolor. Praesent vehicula mauris eros, at dictum felis vehicula eu. Sed velit mi, lobortis quis vehicula placerat, cursus pellentesque ligula. Suspendisse ut odio odio. Aliquam in erat non nibh convallis euismod. Duis convallis, dui in imperdiet viverra, nunc tortor iaculis ante, id accumsan mauris nibh ut nisl. Aenean elit ligula, sollicitudin id est eu, lacinia convallis mauris.',
+Property.create!(developer_id: 1, name: 'Avida Land', permalink: 'avida-land', address: 'I.T. Park, Lahug, Cebu City',
+  location: 'Lahug', status: 'Completed',
   unit_types: 'Studio, 1-Bedroom, 2-Bedroom',
-  studio: true, one_bedroom: true, two_bedroom: true, unit_sizes: '16-30 sq. m.',
-  studio_price: 1000000, one_bedroom_price: 2000000, two_bedroom_price: 3000000)
+  unit_sizes: '16-30 sq. m.',
+  price_range: 'P1.5 - 4.5 M')
+Property.create!(developer_id: 3, name: 'La Guardia Flats 2', permalink: 'la-guardia-2', address: 'I.T. Park, Lahug, Cebu City',
+    location: 'Lahug', status: 'Completed',
+    unit_types: 'Studio',
+    unit_sizes: '18 sq. m.',
+    price_range: 'P1.5 - 2.8 M')
